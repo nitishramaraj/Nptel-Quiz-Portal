0c0113c52c104ccd98ba91e3b78136bf91b21f47
Nitish Ramaraj <nitishramaraj@gmail.com>
2024-10-25 23:26:01 +0530

    footer-fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,9 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <GoogleAnalytics />
-      <body className={`bg-[#e9f2fa] ${noto_sans.className}`}>
+      <body className={`bg-[#e9f2fa] ${noto_sans.className} flex flex-col min-h-screen`}>
         <Header />
-        {children}
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
